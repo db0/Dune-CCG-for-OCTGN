194291d205908e0746e4c8701ec94af930ec7219
Divided by Zer0 <mail@dbzer0.com>
2012-02-10 19:18:12 +0100

    Version 1.0.3. See Changelog

---
+++ README.markdown
@@ -0,0 +1,42 @@
+Dune CCG plugin for OCTGN
+=========================
+
+One of the most interesting and complex and deep CCGs ever devised, within the best Sci-Fi universe ever conceived. The Dune CCG is now available in OCTGN. You want more details? Then [look no further](http://boardgamegeek.com/thread/739664/god-created-dune-ccg-to-train-the-faithful-paul).
+
+Note: This is just the game engine, you'll also need to download [the sets](http://octgn.gamersjudgement.com/viewtopic.php?f=42&t=236). If you've never played the Dune CCG before, you can find [the rulebook here](http://boardgamegeek.com/file/download/c6bw2c3cu/DuneRulebookv2.1.pdf).
+
+Screenshots
+-----------
+(Click for larger size)
+
+Sample screenshot of 1.0.2
+
+[![](http://i.imgur.com/vrLL5l.jpg)](http://i.imgur.com/vrLL5.jpg)
+
+Changelog
+---------
+
+### 1.0.3
+
+* Added Global Phase counter and made the phases global. Reduced phases to 3. Phases now change with ENTER
+* Added player initiative counter. This will eventually be set automatically during the opening interval
+* Added some (unimplemented) puchase/sell Spice and buy favor table actions. Purchase spice will automatically pay as much as the CROE and reset it afterwards
+* Set the OCTGN required version to 3.0.0.8 since I'm using OCTGN3-only functions
+* Added some complex functions for card placement regardless of card size. This will keep the same setup, even if I later decide to change card sizes.
+* Created table setup script for the start. it automatically sets up your homeworld and Dune (even if you don't have it in your hand), shuffles both decks, draws 7 cards in your hand and performs the imperial assembly setup.
+* Setup action requires the player to be on the "Pre-Game Setup phase"
+* Removed the Assembly Setup action, since it's not required anymore
+* Refresh All function now only works during the opening phase
+
+### 1.0.2
+
+* Removed the Add and Remove Solaris and Favour markers. As far as I know, there is no effect that would make you put solaris or favour markers on a card so these just clutter the menu.
+* Fixed bug when adding deferment token and the notice said that you added a Favor token.
+* Made the CROE and SPICE banks global and removed them from each player
+* Playing a card from hand, automatically pays its cost if it has any, or notifies everyone if the player doesn't have enough money.
+* Changed the subdue action name to "subdue/deploy/petition"
+* Replaced the "draw card" function of the Imperial deck with a "Imperial Draw" function which puts the card immediately on the play area face down. with a special "Assembly" token on it.
+* Replaced the "draw many" function of the Imperial deck with a "Setup Assembly" function which puts 3 imperial cards on the assuembly face down with the special "Assembly" token.
+* Petitioning a facedown card with the Assembly card marker, will inform everyone that the card is up for petition. If successful, the player should move the card to his play area. Petitioning a face up card with an Assembly marker, will turn it face down again and inform that the petition was unsuccessful.
+* Deploying a face down card (that doesn't have an "Assembly" marker) with less deferment tokens than its deployment cost will ask the player to pay the difference. It will also notify everyone is a player deploys something with 0 deferment tokens
+* The "refresh all" function will remove Assembly markers from face up cards. This way, you can see which cards have just been put in from the assembly this turn.
--- scripts/scripts1.py
+++ scripts/scripts1.py
@@ -5,33 +5,36 @@ Deferment_Token = ("Deferment_Token", "f8f34145-60a8-4d2c-92e9-25824982944e")
 Assembly = ("Imperial Assembly", "a5634dc5-ffd0-4428-95b5-13c6bb3ff00d")
 
 phases = [
-    'It is currently in the Pre-game Setup Phase',
-    "It is now {}'s Opening Interval- Disengage Phase",
-    "It is now {}'s Opening Interval- Deferment Phase",
-    "It is now {}'s Opening Interval- Initiative Phase",
-    "It is now {}'s House Interval",
-    "It is now {}'s Closing Interval",
-    "It is now {}'s Assembly Administration Phase",
-    "It is now {}'s Hand Administration Phase",
-    "{} finished his/her turn."]
+    '{} is currently in the Pre-game Setup Phase'.format(me),
+    "It is now Opening Interval. Disengage all cards then add Deferment Tokens and finally Assign Initiative",
+    "It is now House Interval",
+    "It is now Closing Interval. Perform Assembly Administration and then Hand Administration"]
 
-phaseIdx = 0
+#---------------------------------------------------------------------------
+# Constants
+#---------------------------------------------------------------------------
+
+import re
 
 loud = 'loud' # So that I don't have to use the quotes all the time in my function calls
 silent = 'silent' # Same as above
+Xaxis = 'x'  # Same as above
+Yaxis = 'y'	 # Same as above
 
-def nextPhase(group, x = 0, y = 0):
-    global phaseIdx
-    phaseIdx += 1
-    showCurrentPhase(group)
 
-def showCurrentPhase(group, x = 0, y = 0):
-    notify(phases[phaseIdx].format(me))
+#---------------------------------------------------------------------------
+# Global variables
+#---------------------------------------------------------------------------
 
-def goToOpeningInterval(group, x = 0, y = 0):
-    global phaseIdx
-    phaseIdx = 1
-    showCurrentPhase(group)
+playerAllegiance = None # Variable to keep track of the player's outfit.
+playerside = None # Variable to keep track on which side each player is
+playeraxis = None # Variable to keep track on which axis the player is
+handsize = 7 # Used when automatically refilling your hand
+
+
+#---------------------------------------------------------------------------
+# General functions
+#---------------------------------------------------------------------------
 
 def num (s): 
 # This function reads the value of a card and returns an integer. For some reason integer values of cards are not processed correctly
@@ -44,14 +47,79 @@ def num (s):
    except ValueError:
       return 0
 
+def chooseSide(): # Called from many functions to check if the player has chosen a side for this game.
+   mute()
+   global playerside, playeraxis
+   if playerside == None:  # Has the player selected a side yet? If not, then...
+      if table.isTwoSided():
+         if me.hasInvertedTable():
+            playeraxis = Yaxis
+            playerside = -1
+         else:
+            playeraxis = Yaxis
+            playerside = 1
+      else:
+         askside = askInteger("On which side do you want to setup?: 1 = Right, 2 = Left, 3 = Bottom, 4 = Top, 0 = None (All your cards will be put in the middle of the table and you'll have to arrange them yourself", 1) # Ask which axis they want,
+         if askside == 1:
+            playeraxis = Xaxis
+            playerside = 1
+         elif askside == 2:
+            playeraxis = Xaxis
+            playerside = -1
+         elif askside == 3:
+            playeraxis = Yaxis
+            playerside = 1
+         elif askside == 4:
+            playeraxis = Yaxis
+            playerside = -1
+         else:
+            playeraxis = None
+            playerside = 0
+
+
+def cwidth(card, divisor = 10):
+# This function is used to always return the width of the card plus an offset that is based on the percentage of the width of the card used.
+# The smaller the number given, the less the card is divided into pieces and thus the larger the offset added.
+# For example if a card is 80px wide, a divisor of 4 will means that we will offset the card's size by 80/4 = 20.
+# In other words, we will return 1 + 1/4 of the card width. 
+# Thus, no matter what the size of the table and cards becomes, the distances used will be relatively the same.
+# The default is to return an offset equal to 1/10 of the card width. A divisor of 0 means no offset.
+   if divisor == 0: offset = 0
+   else: offset = card.width() / divisor
+   return (card.width() + offset)
+
+def cheight(card, divisor = 10):
+   if divisor == 0: offset = 0
+   else: offset = card.height() / divisor
+   return (card.height() + offset)
+
+
 #---------------------------------------------------------------------------
 # Table group actions
 #---------------------------------------------------------------------------
 
+def nextPhase(group, x = 0, y = 0):  
+# Function to take you to the next phase. 
+   mute()
+   if shared.Phase == 3: 
+      shared.Phase = 1 # In case we're on the last phase (Closing Interval), go back to the first game phase (Opening Interval)
+   else: shared.Phase += 1 # Otherwise, just move up one phase
+   showCurrentPhase()
+
+def showCurrentPhase(): # Just say a nice notification about which phase you're on.
+   notify(phases[shared.Phase].format(me))
+
+def goToSetup(group, x = 0, y = 0):  # Go back to the Pre-Game Setup phase.
+# This phase is not rotated with the nextPhase function as it is a way to basically restart the game.
+# It also serves as a control, so as to avoid a player by mistake using the setup function during play.
+   mute()
+   shared.Phase = 0
+   showCurrentPhase() # Remind the players which phase it is now
+
 def flipCoin(group, x = 0, y = 0):
     mute()
-    n = rnd(1, 2)
-    if n == 1:
+    n = rnd(1, 10)
+    if n < 6:
         notify("{} flips heads.".format(me))
     else:
         notify("{} flips tails.".format(me))
@@ -103,11 +171,11 @@ def intrigue(card, x = 0, y = 0):
 def subdue(card, x = 0, y = 0):
     mute()
     faceup = 0
-    card.isFaceUp = not card.isFaceUp # Horrible hack until the devs can allow me to look at facedown card properties.
-    type = card.Type             # Gah!
-    cost = num(card.properties['Deployment Cost']) # GAH!
-    card.isFaceUp = not card.isFaceUp
-    random = rnd(100, 4000)
+    if not card.isFaceUp: # Horrible hack until the devs can allow me to look at facedown card properties.
+       card.isFaceUp = not card.isFaceUp  # Gah!
+       type = card.Type
+       cost = num(card.properties['Deployment Cost']) 
+       card.isFaceUp = not card.isFaceUp # GAH!
 #    notify("{} Deferments. {} Cost(int). Cost(str). {} is the difference.".format(card.markers[Deferment_Token], cost, card.properties['Deployment Cost'], cost - card.markers[Deferment_Token]))
     if card.markers[Assembly] == 0:
         if card.isFaceUp:
@@ -141,6 +209,9 @@ def subdue(card, x = 0, y = 0):
 
 def restoreAll(group, x = 0, y = 0): 
     mute()
+    if shared.Phase != 1: #One can only disengage during the Opening Interval
+      whisper("You can only disengage all cards during the Opening Interval")
+      return
     myCards = (card for card in table
                if card.controller == me
                and card.owner == me)
@@ -149,16 +220,6 @@ def restoreAll(group, x = 0, y = 0):
         if card.markers[Assembly] > 0 and card.isFaceUp:
              card.markers[Assembly] = 0
     notify("{} disengages all his cards.".format(me))
-
-def addSolaris(card, x = 0, y = 0):
-    mute()
-    notify("{} adds 1 Solaris to {}.".format(me, card))
-    card.markers[Solaris] += 1
-
-def subSolaris(card, x = 0, y = 0):
-    mute()
-    notify("{} removes 1 Solaris from {}.".format(me, card))
-    card.markers[Solaris] -= 1
 
 def addSpice(card, x = 0, y = 0):
     mute()
@@ -169,16 +230,6 @@ def subSpice(card, x = 0, y = 0):
     mute()
     notify("{} removes a Spice token from {}.".format(me, card))
     card.markers[Spice] -= 1
-
-def addFavor(card, x = 0, y = 0):
-    mute()
-    notify("{} adds a Favor token to {}.".format(me, card))
-    card.markers[Favor] += 1
-
-def subFavor(card, x = 0, y = 0):
-    mute()
-    notify("{} removes a Favor token from {}.".format(me, card))
-    card.markers[Favor] -= 1
 
 def addDeferment(card, x = 0, y = 0):
     mute()
@@ -209,6 +260,98 @@ def play(card, x = 0, y = 0):
     notify("{} plays {} from his {}.".format(me, card, src.name))
     payCost(card.properties['Deployment Cost'], loud) # Take cost out of the bank, if there is any.
 
+def setup(group):
+# This function is usually the first one the player does. It will setup their homeworld on their side of the table. 
+# It will also shuffle their decks, setup their Assembly and Dune and draw 7 cards for them.
+   if shared.Phase == 0: # First check if we're on the pre-setup game phase. 
+                     # As this function will play your whole hand and wipe your counters, we don't want any accidents.
+      if not confirm("Have bought all the favour and spice you'll need with your bonus solaris?"): return
+      global playerside, playerAllegiance # Import some necessary variables we're using around the game.
+      DuneinHand = 0
+      mute()
+      chooseSide() # The classic place where the players choose their side.
+      me.piles['Imperial Deck'].shuffle() # First let's shuffle our decks now that we have the chance.
+      me.piles['House Deck'].shuffle()
+      for card in group: # For every card in the player's hand... (which should be just their homeworld and possibly some plans)
+         notify("card is {}. Subtype is {}". format (card, card.subtype))
+         if re.search(r'Homeworld', card.Subtype) and card.Type == 'Holding':  # If it's the homeworld...
+            placeCard(card,'SetupHomeworld')
+            playerAllegiance = card.Allegiance # We make a note of the Allegiance the player is playing this time (used later for automatically losing favour)
+         if re.search(r'Program', card.Subtype) and card.Type == 'Plan':  # If it's a program...
+            placeCard(card,'SetupProgram')
+            payCost(card.properties['Deployment Cost']) # Pay the cost of the dude
+         if card.model == '2037f0a1-773d-42a9-a498-d0cf54e7a001': # If the player has put Dune in their hand as well...
+            placeCard(card,'SetupDune')
+            DuneinHand = 1 # Note down that player brought their own Dune, so that we don't generate a second one.
+      if DuneinHand == 0: # If the player didn't bring their own Dune, generate a new one on their side.
+         Dune = table.create("2037f0a1-773d-42a9-a498-d0cf54e7a001", 0, 0, 1, True) # Create a Dune card in the middle of the table.
+         placeCard(Dune,'SetupDune')
+      me.Solaris += 20     
+      refill() # We fill the player's play hand to their hand size (usually 5)
+      notify("{} is playing {}. Their starting Solaris is {} and their Imperial Favour is {}".format(me, playerAllegiance, me.Solaris, me.Favor))  
+      setupAssembly() # Setup the 3 imperial cards which will be our assembly.
+   else: whisper('You can only setup your starting cards during the Pre-Game setup phase') # If this function was called outside the pre-game setup phase
+            
+def placeCard(card,type = None):
+# This function automatically places a card on the table according to what type of card is being placed
+# It is called by one of the various custom types and each type has a different value depending on if the player is on the X or Y axis.
+   if playeraxis == Xaxis:
+#      if type == 'HireAide': # Not implemented yet
+#         card.moveToTable(homeDistance(card) + (playerside * cwidth(card,-4)), 0)
+      if type == 'SetupHomeworld':
+         card.moveToTable(homeDistance(card), cheight(card)* -4 * playerside) # We move it to one side depending on what side the player chose.
+      if type == 'SetupDune':
+         card.moveToTable(homeDistance(card), cheight(card)* -3 * playerside) # We move it to one side depending on what side the player chose.
+         card.isFaceUp = False
+      if type == 'SetupProgram':
+         card.moveToTable(homeDistance(card) + (playerside * cwidth(card,-4)), cheight(card)* -4 * playerside)
+         card.sendToBack()
+         # We move them behind the homeworld
+   elif playeraxis == Yaxis:
+#      if type == 'HireAide':# Not implemented yet
+#         card.moveToTable(0,homeDistance(card) + (playerside * cheight(card,-4)))
+      if type == 'SetupHomeworld':
+         card.moveToTable(cwidth(card)* -4 * playerside,homeDistance(card)) 
+      if type == 'SetupDune':
+         card.moveToTable(cwidth(card)* -3 * playerside,homeDistance(card)) 
+         card.isFaceUp = False
+      if type == 'SetupProgram': 
+         card.moveToTable(cwidth(card)* -4 * playerside,homeDistance(card) + (playerside * cheight(card,-4)))
+         card.sendToBack()
+   else: card.moveToTable(0,0)
+
+def homeDistance(card):
+# This function returns the distance from the middle each player's homeworld will be setup towards their playerSide. 
+# This makes the code more readable and allows me to tweak these values from one place
+   if table.isTwoSided(): return (playerside * cwidth(card) / 2) # players on an inverted table are placed half a card away from their edge.
+   else:
+      if playeraxis == Xaxis:
+         return (playerside * cwidth(card) * 5) # players on the X axis, are placed 5 times a card's width towards their side (left or right)
+      elif playeraxis == Yaxis:
+         return (playerside * cheight(card) * 3) # players on the Y axis, are placed 3 times a card's height towards their side (top or bottom)
+
+def cardDistance(card):
+# This function returns the size of the card towards a player's side. 
+# This is useful when playing cards on the table, as you can always manipulate the location
+#   by multiples of the card distance towards your side
+# So for example, if a player is playing on the bottom side. This function will always return a positive cardheight.
+#   Thus by adding this in a moveToTable's y integer, the card being placed will be moved towards your side by one multiple of card height
+#   While if you remove it from the y integer, the card being placed will be moved towards the centre of the table by one multiple of card height.
+   if playeraxis == Xaxis:
+      return (playerside * cwidth(card))
+   elif playeraxis == Yaxis:
+      return (playerside * cheight(card))
+
+def setHandSize(group): # A function to modify a player's hand size. This is used during Closing Interval when refilling the player's hand automatically.
+   global handsize
+   handsize = askInteger("What is your current hand size?", handsize)
+   if handsize == None: handsize = 7
+   notify("{} sets their hand size to {}".format(me, handsize))
+   
+def refill(group = me.hand): # Refill the player's hand to its hand size.
+   global handsize
+   playhand = len(me.hand) # count how many cards there are currently there.
+   if playhand < handsize: drawMany(me.piles['House Deck'], handsize - playhand, silent) # If there's less cards than the handsize, draw from the deck until it's full.
 
 #------------------------------------------------------------------------------
 # Pile Actions
@@ -227,11 +370,11 @@ def imperialDraw(group = me.piles['Imperial Deck'], times = 1):
     i = 0
     while i < times:
         card = group.top()
-        if me.hasInvertedTable() == True: 
-            group.top().moveToTable(400 - (i * group.top().width()) - (i * 15), -150,True)
+        if playeraxis == Yaxis: 
+            group.top().moveToTable(cwidth(card)* -6 - playerside * i * cwidth(card), cardDistance(card) * 2,True)
             card.markers[Assembly] = 1
         else: 
-            group.top().moveToTable((i * group.top().width()) + (i * 15)- 400, 150,True)
+            group.top().moveToTable(cardDistance(card) * 2, cwidth(card)* -6 - playerside * i * cheight(card),True)
             card.markers[Assembly] = 1
         i += 1
 
@@ -243,10 +386,10 @@ def setupAssembly(group = me.piles['Imperial Deck']):
 def shuffle(group):
   group.shuffle()
 
-def drawMany(group, count = None):
+def drawMany(group, count = None, notification = loud): # This function draws a variable number cards into the player's hand.
     if len(group) == 0: return
     mute()
     if count == None: count = askInteger("Draw how many cards?", 7)
     for c in group.top(count): c.moveTo(me.hand)
-    notify("{} draws {} cards.".format(me, count))
+    if notification == loud : notify("{} draws {} cards to their play hand.".format(me, count)) # And if we're "loud", notify what happened.
 
