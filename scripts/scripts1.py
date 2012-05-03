6d17afc84eff5e36cb552f6f3b6532c01c996c7c
Divided by Zer0 <mail@dbzer0.com>
2012-05-03 17:29:02 +0300

    Adde checking for targeting restrictions (and reporting what they are if they's no match)

--- scripts/scripts1.py
+++ scripts/scripts1.py
@@ -1122,15 +1122,38 @@ def useAbility(card, x = 0, y = 0):
    ### Checking if the card effect requires a target first
    targetC = None
    if re.search(r'Targetted', activeAutoscript):
+      invalidTargets = [] # a variable that holds any type that a card must not be to be valid
+      whatTarget = re.search(r'on([A-Za-z_]+)[-]?', activeAutoscript)
+      if whatTarget: validTargets = whatTarget.group(1).split('_or_') # If we have a list of valid targets, split them into a list, separated by the string "_or_". Usually this results in a list of 1 item.
+      for chkTarget in validTargets: # Now we go through each list item and see if it has more than one condition (Eg, non-desert fief)
+         if re.search(r'_and_', chkTarget): 
+            multiConditionTargets = chkTarget.split('_and_')
+            for chkCondition in multiConditionTargets:
+               invalidCondition = re.search(r'non([A-Za-z]+)', chkCondition) # Do a search to see if in the multicondition targets there's one with "non" in front
+               if invalidCondition: invalidTargets.append(invalidCondition.group(1)) # If there is, move it without the "non" into the invalidTargets list.
+               else: validTargets.append(chkCondition) # Else just move the individual condition to the end if validTargets list
+            validTargets.remove(chkTarget) # Finally, remove the multicondition keyword from the valid list. Its individual elements should now be on this list or the invalid targets one.
+         else:   
+            if re.match(r'non', chkTarget): # If the keyword has "non" in front, it means it's something we need to avoid, so we move it to a different list.
+               invalidTargets.append(chkTarget)
+               validTargets.remove(chkTarget)
       for targetLookup in table:
-         #notify("checking {}".format(targetLookup)) # debug
-         if targetLookup.targetedBy and targetLookup.targetedBy == me: 
-            #notify("Found") # debug
-            targetC = targetLookup
-            break
-      #notify("Checking Target exists") # debug
+         if targetLookup.targetedBy and targetLookup.targetedBy == me:
+            if len(validTargets) == 0: targetC = targetLookup # If we have no target restrictions, any selected card will do.
+            else:
+               for validtargetCHK in validTargets: # look if the card we're going through matches our valid target checks
+                  if re.search(r'{}'.format(validtargetCHK), targetLookup.Type) or re.search(r'{}'.format(validtargetCHK), targetLookup.Subtype):
+                     targetC = targetLookup
+            if len(invalidTargets) > 0: # If we have no target restrictions, any selected card will do as long as it's a valid target.
+               for invalidtargetCHK in invalidTargets: # 
+                  if re.search(r'{}'.format(invalidtargetCHK), targetLookup.Type) or re.search(r'{}'.format(invalidtargetCHK), targetLookup.Subtype):
+                     targetC = None
+            if targetC: break
       if targetC == None: 
-         whisper("You need to target a card before using this action")
+         targetsText = ''
+         if len(validTargets) > 0: targetsText += "\nValid Targets: {}.".format(validTargets)
+         if len(invalidTargets) > 0: targetsText += "\nInvalid Targets: {}.".format(invalidTargets)
+         whisper("You need to target a valid card before using this action{}".format(targetsText))
          return
    ### Checking the activation cost and preparing a relevant string for the announcement
    actionCost = re.match(r"C([ES0])", activeAutoscript)
@@ -1173,14 +1196,6 @@ def GainX(Autoscript, costText, card, n = 1, manual = False, targetCard = None):
       whisper("Gain what?! (Bad autoscript)")
       return
    notify("{} gain {} {}{}.".format(costText, gain * multiplier, action.group(2),extraText))
-
-def onlyRival(Autoscript, owner, manual):
-# Function returns 1 if the card is not only for rivals, or if it is for rivals and the card being activated it not ours.
-# This is then multiplied by the multiplier, which means that if the card activated only works for Rival's cards, our cards will have a 0 gain.
-# This will probably make no sense when I read it in 10 years...
-   byRival = re.search(r'byRival', Autoscript)
-   if not byRival or (byRival and owner != me) or manual: return 1 #manual means that the actions was called by a player double clicking on the card. In which case we always do it.
-   else: return 0
       
 def HoardX(Autoscript, costText, card):
    action = re.search(r'Hoard([0-9]+)Spice', Autoscript)
@@ -1260,6 +1275,14 @@ def per(Autoscript, card = None, n = 1, manual = False): # This function goes th
                                                                             # and some cards may only work when a rival owns or does something.
    else: multiplier = 1 # If the search was not successful, then return a mutliplier of 1.
    return multiplier
+
+def onlyRival(Autoscript, owner, manual):
+# Function returns 1 if the card is not only for rivals, or if it is for rivals and the card being activated it not ours.
+# This is then multiplied by the multiplier, which means that if the card activated only works for Rival's cards, our cards will have a 0 gain.
+# This will probably make no sense when I read it in 10 years...
+   byRival = re.search(r'byRival', Autoscript)
+   if not byRival or (byRival and owner != me) or manual: return 1 #manual means that the actions was called by a player double clicking on the card. In which case we always do it.
+   else: return 0
    
 def autoscriptOtherPlayers(lookup, n = 1):
 # This function is called from other functions in order to go through the table and see if other players have any cards which would be activated by it.
